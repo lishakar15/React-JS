9be69f01eff0e3d7b8a6be7a498546250a3a586f
Lishakar <lishakarj@gmail.com>
2024-08-22 01:41:03 +0530

    Todo app in React

--- React JS Handson/my-react-app/src/Todo.js
+++ React JS Handson/my-react-app/src/Todo.js
@@ -1,14 +1,12 @@
 import React from "react";
+import './Todo.css';
 import { BsFillTrash3Fill } from "react-icons/bs";
+import { useState } from "react";
 
 
-const toDoFunction = () =>{
+const ToDoFunction = () =>{
 
-    const listStyle = {
-        
-    }
-    
-    const toDoList = [
+    let toDoList = [
         {
             id :1,
             isCompleted : true,
@@ -25,23 +23,57 @@ const toDoFunction = () =>{
             taskName:"Go to GYM",
         }
     ];
-    console.log("Inside Todo "+toDoList);
+    if(localStorage.getItem("toDolist") != null){
+        toDoList = JSON.parse(localStorage.getItem("toDolist"));
+    }
+    const [list,setList] = useState(toDoList);
+
+    //Handle check box click
+    const handleCheck = (id)=>{
+        if(list.length>0)
+        {
+            const updatedList = list.map((item) => {
+                if(id===item.id)
+                {
+                    item.isCompleted=!item.isCompleted;
+                }
+                return item;
+            });
+            setList(updatedList);
+            localStorage.setItem("toDoList",JSON.stringify(list));
+        }
+
+    }
+
+    //Handle delete item
+    const handleDelete = (id) =>{
+        const updatedList = list.filter((item)=>{
+            return id !== item.id;
+        });
+        setList(updatedList);
+        localStorage.setItem("toDoList",JSON.stringify(list));
+    }
+
+    const styleToStrike = {
+        textDecoration:'line-through'
+    } 
+
     return (
-        <div className ="App">
-            <ul>
-                {toDoList.map((item)=>{
+        <div className ="todo-container">
+            <h4>To-do List</h4>
+            <ul className ="item-list">
+                {list.map((item)=>{
                     return (
                     <li key={item.id}>
-                        <input type="checkbox" checked={item.isCompleted}></input>
-                        <label>{item.taskName}</label>
-                        <span><BsFillTrash3Fill  /></span>
+                        <input type="checkbox" className="checkBox" checked={item.isCompleted} onChange={()=>handleCheck(item.id)}></input>
+                        <label onDoubleClick={()=>handleCheck(item.id)} className="item-label" style={item.isCompleted===true ? styleToStrike: null}>{item.taskName}</label>
+                        <span className="trash" onClick={()=> handleDelete(item.id)}><BsFillTrash3Fill  /></span>
                     </li>
                     )})
                 }
-
             </ul>
         </div>
 
     );
 }
-export default toDoFunction;
+export default ToDoFunction;
